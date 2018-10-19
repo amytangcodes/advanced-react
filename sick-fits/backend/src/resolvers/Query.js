2f285d22d7af7ef2148864530579467b2bed555e
Amy Tang <amy@pathfactory.com>
2018-10-19 15:56:56 -0400

    Items creation and prisma yoga flow

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,21 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+
+  // async items(parent, args, ctx, info) {
+
+  //   console.log('Getting Items');
+
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+
+  // dogs: function() {}  | this is the long hand version of below:
+  // dogs(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   return global.dogs;
+  // },
+};
 
 module.exports = Query;
